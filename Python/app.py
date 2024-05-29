02ca95ff327d80764f2d1809d1f02537962acd25
Shubham <u20ec015@eced.svnit.ac.in>
2024-05-29 16:50:06 +0530

    uppdated python flask files

--- Python/app.py
+++ Python/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
-from ctypes import cdll, c_double, c_char_p
+from ctypes import cdll, c_double, c_char_p, c_void_p, c_int
 
 app = Flask(__name__)
 
 # Load the shared library
-lib = cdll.LoadLibrary('./cpp/your_cpp_library.so')
+lib = cdll.LoadLibrary('../CPP/cpp_library.so')
 
 # Define the MenuItem class
 class MenuItem:
@@ -17,6 +17,52 @@ class MenuItem:
 
     def getPrice(self):
         return lib.MenuItem_getPrice(self.price)
+
+# Define the Table class
+class Table:
+    def __init__(self, number):
+        self.obj = lib.Table_new(number)
+
+    def getNumber(self):
+        return lib.Table_getNumber(self.obj)
+
+# Define the Order class
+class Order:
+    def __init__(self, tableNumber):
+        self.obj = lib.Order_new(tableNumber)
+
+    def addItem(self, item):
+        lib.Order_addItem(self.obj, item.obj)
+
+    def getTotal(self):
+        return lib.Order_getTotal(self.obj)
+
+    def applyDiscount(self, percentage):
+        lib.Order_applyDiscount(self.obj, c_double(percentage))
+
+    def setSpecialRequest(self, request):
+        lib.Order_setSpecialRequest(self.obj, c_char_p(request.encode('utf-8')))
+
+    def getStatus(self):
+        return lib.Order_getStatus(self.obj)
+
+    def setStatus(self, status):
+        lib.Order_setStatus(self.obj, status)
+
+# Define the OrderManager class
+class OrderManager:
+    def __init__(self):
+        self.obj = lib.OrderManager_new()
+
+    def placeOrder(self, order):
+        lib.OrderManager_placeOrder(self.obj, order.obj)
+
+    def updateOrderStatus(self, orderId, status):
+        lib.OrderManager_updateOrderStatus(self.obj, orderId, status)
+
+    def getOrderHistory(self):
+        # This method would need to be implemented depending on how you want to handle order history in Python
+        pass
 
 @app.route('/')
 def index():
